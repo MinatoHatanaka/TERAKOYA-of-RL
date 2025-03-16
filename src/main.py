b618fe8196331d8ef73fbddd33906e561c4f32bd
MinatoHatanaka <h.minato0173@gmail.com>
2025-03-17 01:06:48 +0900

    modify main

--- src/main.py
+++ src/main.py
@@ -7,7 +7,9 @@ print("観測データ:", obs)
 
 episode_over = False
 while not episode_over:
-    action = env.action_space.sample()
+    #action = env.action_space.sample()
+    cart_pos, cart_vel, pole_angle, pole_vel = obs
+    action = 1 if pole_angle > 0 else 0
     obs, reward, terminated, truncated, info = env.step(action)
 
     episode_over = terminated or truncated
